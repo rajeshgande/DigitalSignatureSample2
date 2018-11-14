993fc52596b749c97d015fca56eab4f35343ab00
rajeshg11 <rajesh.gande@omnicell.com>
2018-11-14 13:41:39 -0800

    some more changes

--- DigitalSignatureSample2/DigitalSignatureHelper.cs
+++ DigitalSignatureSample2/DigitalSignatureHelper.cs
@@ -76,5 +76,75 @@ namespace DigitalSignatureSample2
 
             return csp.SignHash(hash, CryptoConfig.MapNameToOID("SHA1"));
         }
+
+        public void InstallCertificate()
+        {
+            var base64Encoded = @"MIIKVQIBAzCCChEGCSqGSIb3DQEHAaCCCgIEggn+MIIJ+jCCBhsGCSqGSIb3DQEH
+AaCCBgwEggYIMIIGBDCCBgAGCyqGSIb3DQEMCgECoIIE / jCCBPowHAYKKoZIhvcN
+AQwBAzAOBAgcW2puxiIXpQICB9AEggTYMXLViaPlnOBV3 / 03VSUR0OJVb7cIfP5K
+JcsPMWpAO2oK1dqtn4luYiltIb3VV0g69 + ZQ2CVqGzdpNqLyfUbG9z5J4UhyzZKk
+tItjTI1csHnJrrHurqnqhGnTOVBSp5axS8AAA5b402T6VKXEi + Qf1gpyXB7PqZch
+4kJkhfJ7EvK9bsY3XYINvguYKp5tQ1KM7E8VuDfMJdijfocuT7ax1ggfZXLOzAwh
+7fVOjvs4ZWKB / NB992uY0wagb0JN9KZbTEGv83Tbm5FUd6YZtUp6 / gDaHFZqbmD3
+4kQhcWQlOMOSaPzetQRo5ylGANUEJDvhpVd390ZD + nYVC9hMm2fv1wOxrcLFkm7A
++ AdRkHruqulOqBsHHWMg80vlNoX3DPX5c3qlIHovDGLNApoF48UNAPnxN8Aqwl7T
+R7yX3nul4adLMkNcVgfdlwEkblbHQzpwMZ3l4AJxt7iJPH0KKxx1A / JKwIMpk8hC
+Rm3sVKLwVG4sKxYKtCZJQQV / JttLmV2w60z / LkW4IyP6Qpmm1D / 5EG / 8ycDnohXL
+vQD3TCfHUiyB14uWudd5r + 6lQSSB547LzButR3blOn2lWRk8k6NY0C7CC / mu7ARQ
+mivGNH9vKHCxJ8r / Z + omsWsEs2X / WY6b / 2J4JIvwFhnaJrD28BqIUXDVcZc0dJza
+        CqNOCM3GfQojXAttNt4Is5ZHVXPel7z5lUb32xT5HO4IESkuAX5TuWB1MW2kyz6t
+MEr / nyA2Q98Tz0g5j5Pj + 3POO / ZTvHaT9tBFn877qNRVrbfx1njaq7tOFxXjWwgk
+Zky0EbcORcZS7p6gMUu30eBwFPhdYO25kErpI / MjJBXfdutVHUyW9nXa1xCpwGF2
+kBydX5MMvEQbMSMY7Qeo4pPgzy5UvPWLP7DJdUcgHjQPKRsGAGbBnDOYchSM5h30
+zso1K5US6aBwuGZSLG0Lgvg6k + TocsjgP3WSClFqt8aXjn4u5Sxh0ftbecyQcleH
+B2u1hQZbt00w1URmcMvdajDJZzfjGhPj1DJ2faZ5hzZJDyF6AVF + kgmaRbEudbuf
+OpivAeC8ZZ5Ae5O5hhya6P0IWHlFMmKKo4pJffhEXt3sCQkVT5GR1PBWQBUXHwHQ
+uAew9tsE7FVO62Y3dmO2NN9S8OTxGErlOhoEqsKmIps5m + wmpgOgVf / Q7aRPMfzW
+kWjhl5TpQn8KXHr1yEDpaHAySDh0aYgqFKgCrPhBuwEEbtWxECLejJjbnMP9 / QZU
+OIwGV7WPuwJ1zXdnO8wB1SiHmO9 + j + YML5NrHz1 + CWniNkW5xCcF + o6dcZzrKov5
+y3ZCIh8FWMe47Kc / c0cCmQx8417EaqyOlKJSSygV4Hrq0Q8MHPEoO94skDsxAlYt
+40jnxIImvwIqavsuBfRh6VMAl581Jz + 8yqquRy8 / ebu7Lb + QSqHD8wjOztsQUj60
+Ms9asDW4nr66KHsEBiPIft / CmyuW9N6eFOozFMJe1F / k88qZIMiqKuc2m1LhOfEr
+I5ziCFNF4jrr6M8zValaDfCq1Vb2uW / XXR7xIKhE4SuDx9bfeRMQH0HF9Va0DsCO
++ cXTHlX0P8HN8 / 6lGCeuc + MN5jaGMOND0pM1LQCJxAdaxBfZWJLjWawsWtN77GP6
+3yV2SqkhQKw3VWLb1vNmWDGB7jANBgkrBgEEAYI3EQIxADATBgkqhkiG9w0BCRUx
+BgQEAQAAADBdBgkqhkiG9w0BCRQxUB5OAGwAcAAtADUAZgAzADQAYwBmAGYAYQAt
+AGUAMAAyADgALQA0AGMANQBjAC0AOQAzAGUAYQAtAGMAOQBkAGEANQA4ADgANQAz
+ADcAYgA4MGkGCSsGAQQBgjcRATFcHloATQBpAGMAcgBvAHMAbwBmAHQAIABSAFMA
+QQAgAFMAQwBoAGEAbgBuAGUAbAAgAEMAcgB5AHAAdABvAGcAcgBhAHAAaABpAGMA
+IABQAHIAbwB2AGkAZABlAHIwggPXBgkqhkiG9w0BBwagggPIMIIDxAIBADCCA70G
+CSqGSIb3DQEHATAcBgoqhkiG9w0BDAEGMA4ECClYw9 / 2mUjQAgIH0ICCA5ABWybI
+YHenIDKgfk3xCii8sa57kqQFz / omE1tRGeyEHEJ2Wbg4pKz0enqrmNhaAEVtZ1pn
+xWw2oqlH4ldSB0Sw3wCovwq2RTUhu0Qtnci1vqStqVA / 5 / UvuMrdEB3q8aG7r6q9
+yjBDEVAoa + ZYvEnYAozkGaigTrmBq0wZAuB0CRcmMO4EYZLaJJ1lG74E +/ fyR0L4
+UwguwGDSt1t06nsnplk4Nq9CX5CXNd9tAw6Naw4xPcLiw13ZzdEEx1DBveY / 4Gt3
+jmFQGdQKUvdA7ghc1wl614kB9APH3eeiC3WE / X8NRLj5kXQAqo3rC3PQwJ2SGpmI
+XSRozccv2S8SiD / 5ec0Ys0eIYGO74vkSxavjEHLzwrDAvakV9NgfMXVP0Nnnrkl3
+4ip3ogXNRBIbRGIf3Ty3D7REMnn5wsTdElwcei0o8wXM55tGCgPckEb4wbIyyGBY
+6nPj8yjjzcSg5joBWua4R8vha / pcShAeXs / gKFbeb4omRtCluaV / ErE9f2CM6H86
+7xTBlXGCI89xGiJXqf / TI5sX2Y7VLyw9M / BspEOSSAtYSa0TqVrPjRcoy6UAOf1J
+DukTZU + jb / +YALqzu7mc / qy0QFdVNf7YtYhzwEyiu45FL17SoyntaqAWYfnXInhM
+p9XoX6eWd0ssbXpygphYZil0kcH2JNEayAeeqoe7bOe64k + TyyHGoCszuMABLQhx
+lhGMmwMOGJXHLOaJeQKnKLAecLkIqGGVMWAEHmgk2N9lq5t4LX3rtuqCw9sVEq3H
+uqrR / 3YNSV9vzFNMCvuYEo0TybritQClpe / WUhHfrOZLjie77PzTTa3w / PlWPaRs
+Hywxb +/ NQ0jhMl3JuuUCnSMtUKIOVKeMpJ3sg0mC2bhd7pdUn00VcqO + BDvvDyRY
+wlLnpWdeySJ8u6aYtYRcga7sIp6OmmljTDF6ik5GMO6 + yD02PBCVD / g1JWKFyMgw
+gTsZ + 5WrvAqkywyMmeMM4q5PBNuw / THgYRTRdGfBmWifqq5fuhmlGlXXMss3k3dI
+hadrZzTJzahw7baBlo7ofT9OgGEIOIFo3InaB3TcszRfNfKJ2Pvsd9b4cVSmGqE3
+VGuRV0Iz3kWg3SoyqCnCrURYSnI6krnWngxmXv8AlOToyfEmrmBlr8ZR11bFybBk
+0 / aScvQk9yrd5Xhf2J5q2IQgh4ImpkHoOM + C4RU0BvE7TjXhDpGbqbD7cZcwOzAf
+MAcGBSsOAwIaBBSaM2UkWZLNvECW9i1J4FCToZgbpQQU3yAHcbm3L2v4VLYw / Cnn
+EAZqDkACAgfQ";
+
+            X509Certificate2 certificate = new X509Certificate2(
+                Convert.FromBase64String(base64Encoded),
+                string.Empty,
+                X509KeyStorageFlags.MachineKeySet);
+            X509Store store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(certificate);
+            store.Close();
+        }
     }
 }
--- DigitalSignatureSample2/Program.cs
+++ DigitalSignatureSample2/Program.cs
@@ -1,3 +1,13 @@
+// --------------------------------------------------------------------------
+//  <copyright file="Program.cs" company="Omnicell Inc.">
+//      Copyright (c) 2016 Omnicell Inc. All rights reserved.
+// 
+//      Reproduction or transmission in whole or in part, in 
+//      any form or by any means, electronic, mechanical, or otherwise, is 
+//      prohibited without the prior written consent of the copyright owner.
+//  </copyright>
+// --------------------------------------------------------------------------
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,46 +23,55 @@ namespace DigitalSignatureSample2
         static void Main(string[] args)
         {
             bool prompt = args.Length < 1;
-         
-            string command = prompt ? InputPrompt("action") : args[0].ToLower();
-            switch (command)
+            ConsoleKeyInfo cki;
+            do
             {
-                case "genkeycerts":
-                    Console.WriteLine("not implimented.");
-                    break;
+                string command = prompt ? InputPrompt("action") : args[0].ToLower();
+                switch (command)
+                {
+                    case "genkeycerts":
+                        Console.WriteLine("not implimented.");
+                        break;
 
-                case "sign":
-                    string sampleText = prompt ? InputPrompt("text") : args[1].ToLower();
-                    var helper1 = new DigitalSignatureHelper();
-                    var sign = helper1.GetDigitalSignature(sampleText, "testSigningCertificate");
-                    Console.WriteLine(sign);
-                    break;
+                    case "sign":
+                        string sampleText = prompt ? InputPrompt("text") : args[1].ToLower();
+                        var helper1 = new DigitalSignatureHelper();
+                        var sign = helper1.GetDigitalSignature(sampleText, "testSigningCertificate");
+                        Console.WriteLine(sign);
+                        break;
 
-                case "verify":
+                    case "verify":
 
-                    string sampleText2 = prompt && args.Length < 1 ? InputPrompt("text") : args[1].ToLower();
-                    string signtext = prompt && args.Length < 2 ? InputPrompt("signature") : args[2].ToLower();
-                    var helper2 = new DigitalSignatureHelper();
-                    var result = helper2.VerifySignature(sampleText2, signtext, "testSigningCertificate");
-                    Console.WriteLine(result);
-                    break;
+                        string sampleText2 = prompt && args.Length < 1 ? InputPrompt("text") : args[1].ToLower();
+                        string signtext = prompt && args.Length < 2 ? InputPrompt("signature") : args[2].ToLower();
+                        var helper2 = new DigitalSignatureHelper();
+                        var result = helper2.VerifySignature(sampleText2, signtext, "testSigningCertificate");
+                        Console.WriteLine(result);
+                        break;
 
-                case "signAndverify":
+                    case "signAndverify":
 
-                    string sampleText3 = prompt && args.Length < 1 ? InputPrompt("text") : args[1].ToLower();
-                    var helper3 = new DigitalSignatureHelper();
-                    var sign3 = helper3.GetDigitalSignature(sampleText3, "testSigningCertificate");
-                    var result3 = helper3.VerifySignature(sampleText3, sign3, "testSigningCertificate");
-                    Console.WriteLine(result3);
-                    break;
+                        string sampleText3 = prompt && args.Length < 1 ? InputPrompt("text") : args[1].ToLower();
+                        var helper3 = new DigitalSignatureHelper();
+                        var sign3 = helper3.GetDigitalSignature(sampleText3, "testSigningCertificate");
+                        var result3 = helper3.VerifySignature(sampleText3, sign3, "testSigningCertificate");
+                        Console.WriteLine(result3);
+                        break;
 
-                default:
-                    break;
-            }
+                    case "installcert":
 
-            Console.WriteLine("Enter Any key to exit...");
-            Console.ReadKey();
+                        var helper4 = new DigitalSignatureHelper();
+                        helper4.InstallCertificate();
+                        break;
 
+                    default:
+                        Console.WriteLine("invalid action");
+                        break;
+                }
+
+                Console.WriteLine("Enter Any key to retry. Esc to exit.");
+                cki = Console.ReadKey();
+            } while (cki.Key != ConsoleKey.Escape);
         }
 
         private static string InputPrompt(string key)
